3dc068fb12663a335239efaf31cd802d666b13fc
will123195 <will123195@gmail.com>
2017-11-19 10:45:10 -0700

    add donation addresses

--- layouts/website/website.node.js
+++ layouts/website/website.node.js
@@ -6,6 +6,11 @@ module.exports = function ($) {
   $.css.push('//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css')
   $.js.push('//code.jquery.com/jquery-2.1.4.min.js')
 
+  $.donationAddress = {
+    LTC: 'M8Qj4mYWX5jtbxFVjFpZ3Ln2b4Jvvuy7E9',
+    ETH: '0xe639c887349C45e69B0e03AE3243ED4A7C2F7cf3'
+  }
+
   $.head = $.templates['partials/head']()
   $.layout('html5')
 }
