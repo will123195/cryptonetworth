dcb97e6b3a589f050086187f82716da936e956dd
will123195 <will123195@gmail.com>
2017-12-07 21:44:01 -0700

    Add pre-fork bitcoin (i.e. casascius)

--- pages/home/home.node.js
+++ pages/home/home.node.js
@@ -26,11 +26,42 @@ function formatPrice(price) {
   return '$' + Number(price).toPrecision(3)
 }
 
+function getChange(data) {
+  const currentPrice = Number(data.price_usd)
+  const percentChange = 1 + (Number(data.percent_change_7d) / 100)
+  const startPrice = percentChange ? currentPrice / percentChange : currentPrice
+  return currentPrice - startPrice
+}
+
+function getBTCPreForkValues(ticker) {
+  const defaultRow = { price_usd: 0 }
+  const btc = ticker.find(row => row.symbol === 'BTC') || defaultRow
+  const bch = ticker.find(row => row.symbol === 'BCH') || defaultRow
+  const btg = ticker.find(row => row.symbol === 'BTG') || defaultRow
+  const market_cap_usd = Number(btc.market_cap_usd)
+    + Number(bch.market_cap_usd)
+    + Number(btg.market_cap_usd)
+  btc.change = getChange(btc)
+  bch.change = getChange(bch)
+  btg.change = getChange(btg)
+  const price_usd = Number(btc.price_usd) + Number(bch.price_usd) + Number(btg.price_usd)
+  const change = btc.change + bch.change + btg.change
+  const startPrice = price_usd - change
+  const percentChange = ((change / startPrice) * 100).toFixed(2)
+  return {
+    price_usd,
+    market_cap_usd,
+    percent_change_7d: percentChange > 0 ? `+${percentChange}` : percentChange,
+    delta: (change > 0) ? 'positive' : 'negative'
+  }
+}
+
 module.exports = function ($) {
   $.title = 'My Crypto-Currency Net Worth'
   $.layout('website')
   getTicker().then(ticker => {
-    $.ticker = ticker.map(row => {
+    $.ticker = []
+    ticker.map(row => {
       const data = {
         rank: row.rank,
         id: row.id,
@@ -51,7 +82,24 @@ module.exports = function ($) {
         data.delta = ''
         data.change = '-'
       }
-      return data
+      // avoid confusion between bitcoin and bcash
+      if (data.symbol === 'BCH') {
+        data.name = 'Bcash'
+      }
+      // calculate bitcoin pre-fork values (i.e. casascius)
+      if (data.symbol === 'BTC') {
+        const btcPreFork = getBTCPreForkValues(ticker);
+        $.ticker.push({
+          rank: '-',
+          name: 'Bitcoin (Pre-fork)',
+          symbol: 'BTC+BCH+BTG',
+          price: btcPreFork.price_usd,
+          priceFormatted: formatPrice(btcPreFork.price_usd),
+          delta: btcPreFork.delta,
+          change: btcPreFork.percent_change_7d + '%'
+        })
+      }
+      $.ticker.push(data)
     })
     $.render()
   })
